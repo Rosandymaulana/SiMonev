f2e0a410942789151bb826cd215290bdc48f1f4b
Rosandymaulana <rosandy1118@gmail.com>
2023-05-30 22:26:11 +0700

    Add migration report table

---
+++ database/migrations/2023_05_30_151824_create_report_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id('id_report');
+            $table->unsignedBigInteger('id_usulan');
+            $table->integer('realisasi');
+            $table->date('tgl_pelaksanaan');
+            $table->string('keterangan', 100);
+            $table->timestamps();
+
+            $table->foreign('id_usulan')->references('id_usulan')->on('usulan')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report');
+    }
+};
