99408a607b69d3feb67eea8bda0b7a8df8e47d8c
Rosandymaulana <rosandy1118@gmail.com>
2023-06-11 14:09:59 +0700

    update tampilan pengusul

--- resources/views/pages/Penyusul/progress_usulan.blade.php
+++ resources/views/pages/Penyusul/progress_usulan.blade.php
@@ -4,9 +4,11 @@
 <html lang="en">
 
 <head>
+
 </head>
 
 <body>
+
     <!-- Tabel Penyusul -->
     <section class="section dashboard">
         <div class="row">
@@ -21,22 +23,10 @@
                                         <tr>
                                             <th scope="col">ID Usulan</th>
                                             <th scope="col">NO</th>
-                                            <th scope="col">Tgl Usulan</th>
-                                            <th scope="col">Fraksi</th>
-                                            <th scope="col">Pengusul</th>
                                             <th scope="col">Usulan</th>
-                                            <th scope="col">Masalah</th>
-                                            <th scope="col">Prioritas Usulan</th>
-                                            <th scope="col">Alamat</th>
                                             <th scope="col">Kelurahan</th>
-                                            <th scope="col">opd_tujuan_awal</th>
                                             <th scope="col">opd_tujuan_akhir</th>
-                                            <th scope="col">Status</th>
-                                            <th scope="col">Volume</th>
-                                            <th scope="col">Satuan</th>
-                                            <th scope="col">Harga Satuan</th>
-                                            <th scope="col">Nilai Usulan</th>
-                                            <th scope="col">Nilai Akomodir</th>
+                                            <th scope="col">Realisasi</th>
                                             <th scope="col">Actions</th>
                                         </tr>
                                     </thead>
@@ -45,22 +35,15 @@
                                         <tr>
                                             <td>{{ $item->id_usulan }}</td>
                                             <td>{{ $item->no }}</td>
-                                            <td>{{ $item->tgl_usulan }}</td>
-                                            <td>{{ $item->fraksi }}</td>
-                                            <td>{{ $item->pengusul }}</td>
                                             <td>{{ $item->usulan }}</td>
-                                            <td>{{ $item->masalah }}</td>
-                                            <td>{{ $item->prioritas_usulan }}</td>
-                                            <td>{{ $item->alamat }}</td>
-                                            <td>{{ $item->nama_wilayah }}</td>
-                                            <td>{{ $item->opd_tujuan_awal }}</td>
+                                            <td>{{ $item->kelurahan }}</td>
                                             <td>{{ $item->opd_tujuan_akhir }}</td>
-                                            <td>{{ $item->status }}</td>
-                                            <td>{{ $item->volume }}</td>
-                                            <td>{{ $item->id_satuan }}</td>
-                                            <td>{{ $item->harga_satuan }}</td>
-                                            <td>{{ $item->nilai_usulan }}</td>
-                                            <td>{{ $item->nilai_akomodir }}</td>
+                                            <td>
+                                                @if ($latestReports && $latestReports->id_usulan == $item->id_usulan)
+                                                {{-- Tampilkan informasi dari $latestReports --}}
+                                                {{ $latestReports->realisasi }}
+                                                @endif
+                                            </td>
                                             <td class="d-flex justify-content-evenly px-3">
                                                 <a href="{{ url('penyusul/'. 'tabel-usulan/' .$item->id_usulan. '/laporkan-data') }}"
                                                     class="edit px-1" data-toggle="toggle">
@@ -68,22 +51,6 @@
                                                         Laporkan Progress
                                                     </button>
                                                 </a>
-                                                {{-- <a
-                                                    href="{{ url('/penyusul/tabel-usulan/' . $item->id_usulan . '/edit') }}"
-                                                    class="edit px-1" data-toggle="toggle">
-                                                    <button type="button" class="btn btn-sm btn-success">
-                                                        Laporkan Progress
-                                                    </button>
-                                                </a> --}}
-                                                {{-- <form action="{{ url('super-admin'.'/'.'penyusul'.'/') }}"
-                                                    method="post">
-                                                    {{ method_field('DELETE') }}
-                                                    {{ csrf_field() }}
-                                                    <button onclick="return confirm('Confirm Delete')" type="submit"
-                                                        class="btn btn-sm btn-danger btndelete">
-                                                        delete
-                                                    </button>
-                                                </form> --}}
                                             </td>
                                         </tr>
                                         @endforeach
